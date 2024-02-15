30415835688ad9d9c7965ab62f0d8f73794b068b
Thomas Zhu <thomas.zhu.sh@hotmail.com>
2024-02-15 23:28:24 +0000

    set gradnorm for logging

--- textual_inversion.py
+++ textual_inversion.py
@@ -975,7 +975,7 @@ def main():
                     accelerator.clip_grad_norm_(text_encoder.get_input_embeddings().parameters(), args.max_grad_norm)
                     grad_norm = accelerator.unwrap_model(text_encoder).get_input_embeddings().weight.grad.detach().norm(2).item()
                 else:
-                    grad_norm = None
+                    grad_norm = 0.0
 
                 optimizer.step()
                 lr_scheduler.step()
@@ -1043,7 +1043,7 @@ def main():
                             text_encoder, tokenizer, unet, vae, args, accelerator, weight_dtype, epoch
                         )
 
-            logs = {"loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0], "grad": grad_norm}
+            logs = {"loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0], "gradnorm": grad_norm}
             progress_bar.set_postfix(**logs)
             accelerator.log(logs, step=global_step)
 
