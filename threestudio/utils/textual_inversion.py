1aaa66a8f60802ce56479d53df52addf78e5f864
Thomas Zhu <thomas.zhu.sh@hotmail.com>
2024-02-19 04:50:00 +0000

    omegaconf weirdness

--- threestudio/utils/textual_inversion.py
+++ threestudio/utils/textual_inversion.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from diffusers.loaders import TextualInversionLoaderMixin
+from omegaconf import OmegaConf
 
 import threestudio
 from threestudio.utils.typing import *
@@ -16,6 +17,10 @@ def load_textual_inversion(
     """Load Textual Inversion embeddings into `tokenizer` and `text_encoder`."""
 
     threestudio.info(f"Loading textual inversion embeddings from {pretrained_model_name_or_path}")
+
+    # If pretrained_model_name_or_path is an OmegaConf list config type, it is not registered as an instance of `list`
+    if OmegaConf.is_config(pretrained_model_name_or_path):
+        pretrained_model_name_or_path = OmegaConf.to_container(pretrained_model_name_or_path)
     
     # HACK: calling the `TextualInversionLoaderMixin::load_textual_inversion` method not from a pipeline.
     # This mixin class was intended to be inherited by a DiffusionPipeline class, but here it is used as a bare class.
