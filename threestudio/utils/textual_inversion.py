507bf4ed62a2ad7c7852d63f7d10fd5dddf4f173
Thomas Zhu <thomas.zhu.sh@hotmail.com>
2024-02-19 03:58:28 +0000

    add textual inversion for threestudio prompt processors

---
+++ threestudio/utils/textual_inversion.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+from diffusers.loaders import TextualInversionLoaderMixin
+
+import threestudio
+from threestudio.utils.typing import *
+
+
+def load_textual_inversion(
+    pretrained_model_name_or_path: Union[str, List[str], Dict[str, torch.Tensor], List[Dict[str, torch.Tensor]]],
+    tokenizer: nn.Module,
+    text_encoder: nn.Module,
+    token: Optional[Union[str, List[str]]] = None,
+    **kwargs,
+):
+    """Load Textual Inversion embeddings into `tokenizer` and `text_encoder`."""
+
+    threestudio.debug(f"Loading textual inversion embeddings from {pretrained_model_name_or_path}")
+    
+    # HACK: calling the `TextualInversionLoaderMixin::load_textual_inversion` method not from a pipeline.
+    # This mixin class was intended to be inherited by a DiffusionPipeline class, but here it is used as a bare class.
+    # This might fail on future versions of diffusers, and an alternative is to simply copy code from
+    # `diffusers/loaders/textual_inversion.py` that loads textual inversion to a tokenizer and a text encoder.
+    loader = TextualInversionLoaderMixin()
+
+    # pretend to be a DiffusionPipeline
+    loader.components = {}
+
+    loader.load_textual_inversion(
+        pretrained_model_name_or_path,
+        token=token,
+        tokenizer=tokenizer,
+        text_encoder=text_encoder,
+        **kwargs
+    )
